1d4bd51d5470b77bc676335528a42311e1a8d69a
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-10-13 14:13:32 -0500

    Update trial vars to match actual experiment

--- demos/psychopy/run.py
+++ demos/psychopy/run.py
@@ -5,8 +5,8 @@ from pprint import pprint
 from dynamic_mask import DynamicMask
 
 # Trial settings
-QUESTION_DURATION = 2.0
-PAUSE_DURATION = 1.0
+QUESTION_DURATION = 1.5
+PAUSE_DURATION = 0.2
 MASK_REFRESH_RATE = 0.01
 TIMEOUT_DURATION = 2.0
 keyboard_keys = dict(y='yes', n='no')
@@ -30,9 +30,9 @@ WORD_DURATION = word.getDuration()
 question.draw()
 win.flip()
 core.wait(QUESTION_DURATION)
-win.flip()   # clears the screen during pause
+win.flip()         # clears the screen during pause
 core.wait(PAUSE_DURATION)
-word.play()  # runs in background; does not wait
+word.play()        # runs in background; does not wait
 stopwatch.reset()  # start the timer at word onset
 while stopwatch.getTime() < TIMEOUT_DURATION:
     response = event.getKeys(keyList=keyboard_keys.keys(),
@@ -45,7 +45,7 @@ while stopwatch.getTime() < TIMEOUT_DURATION:
     win.flip()
     core.wait(MASK_REFRESH_RATE)
 
-key, rt = response[0] if response else ('NONE', TIMEOUT_DURATION * 1000)
+key, rt = response[0] if response else ('NONE', TIMEOUT_DURATION)
 response = keyboard_keys.get(key, key)
 is_correct = int(response == correct_answer)
 feedback[is_correct].play()
@@ -56,7 +56,7 @@ trial_data = dict(
     correct_answer=correct_answer,
     key=key,
     response=response,
-    rt=rt * 1000,  # clock times in ms
+    rt=rt * 1000,  # convert seconds to milliseconds
     is_correct=is_correct
 )
 
