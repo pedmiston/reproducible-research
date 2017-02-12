496f0b9a3b6df6e5af6d59ce602cfc1140e8d52f
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-02-12 13:12:10 -0600

    First stab at section 2 on dynamic documents

--- presentation.R
+++ presentation.R
@@ -3,9 +3,24 @@ library(tidyverse)
 library(grid)
 library(gridExtra)
 
-img <- function(stem) {
-    file.path("img", paste0(stem, ".png")) %>%
-        png::readPNG() %>%
-        grid::rasterGrob()
+img <- function(stem, ext = ".png", draw = FALSE) {
+  if (ext == ".png") {
+    image_reader <- png::readPNG
+  } else if (ext == ".jpg") {
+    image_reader <- jpeg::readJPEG
+  } else {
+    stop(paste("unknown image ext", ext))
+  }
+  
+  grob <- file.path("img", paste0(stem, ext)) %>%
+    image_reader() %>%
+    grid::rasterGrob()
+  
+  if (draw == TRUE) {
+    grid::grid.newpage()
+    grid::grid.draw(grob)
+  } else {
+    return(grob)
+  }
 }
 
